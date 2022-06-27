48f1126af4f286648d0ce6cd8a9ae8a98a47e9a0
cfarl <lutitus@gmail.com>
2022-06-26 22:35:17 -0300

    Fontes após concluir o estudo da Section 12 - busca de produtos por categoria

--- angular-ecommerce/src/app/app.module.ts
+++ angular-ecommerce/src/app/app.module.ts
@@ -1,10 +1,23 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
-import { HttpClientModule } from '@angular/common/http' ;
+import { HttpClientModule } from '@angular/common/http';
 import { ProductService } from './services/product.service';
+
+import { Routes, RouterModule} from '@angular/router';
+
+//---------------------------------------------------
+// Define rotas para a aplicação
+//---------------------------------------------------
+const routes: Routes = [
+  {path: 'category/:id', component: ProductListComponent},
+  {path: 'category', component: ProductListComponent},
+  {path: 'products', component: ProductListComponent},
+  {path: '', redirectTo: '/products', pathMatch: 'full'},
+  {path: '**', redirectTo: '/products', pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [
@@ -12,6 +25,7 @@ import { ProductService } from './services/product.service';
     ProductListComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),  // Habilita o uso das rotas
     BrowserModule,
     HttpClientModule
   ],
--- angular-ecommerce/src/app/services/product.service.ts
+++ angular-ecommerce/src/app/services/product.service.ts
@@ -1,22 +1,26 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 import { Product } from '../common/product';
-import { map } from 'rxjs';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  private baseUrl = 'http://localhost:8080/api/products' ;
+  private baseUrl = 'http://localhost:8080/api/products';
 
   constructor(private httpClient: HttpClient) { }
 
-  getProductList(): Observable<Product[]> {
-    return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
+  getProductList(theCategoryId: number): Observable<Product[]> {
+
+    // need to build URL based on category id 
+    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
+
+    return this.httpClient.get<GetResponse>(searchUrl).pipe(
       map(response => response._embedded.products)
-    ) ;
+    );
   }
 }
 
@@ -25,5 +29,3 @@ interface GetResponse {
     products: Product[];
   }
 }
-
-
